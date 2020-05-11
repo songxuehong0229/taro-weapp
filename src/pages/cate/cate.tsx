2c904d5caa4012fc5f046c3cce11c20b2f7bde12
songxuehong <xuehong.song@cue.group>
2020-05-11 16:08:25 +0800

    init

---
+++ src/pages/cate/cate.tsx
@@ -0,0 +1,35 @@
+import Taro, { Component, Config } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+// import './home.scss'
+
+export default class Index extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  /**
+   * 指定config的类型声明为: Taro.Config
+   *
+   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+   */
+  config: Config = {
+    navigationBarTitleText: '分类'
+  }
+
+  render () {
+    return (
+      <View className='cate'>
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+}
